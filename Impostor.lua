7c7ca57a5e70e07567b502605cfd295938a92ad3
q2674791739 <2037991739@qq.com>
2026-09-14 21:16:09 +0800

    Update Impostor.lua

--- Impostor.lua
+++ Impostor.lua
@@ -1,4 +1,4 @@
-local repo = "https://raw.githubusercontent.com/Q2674791739/UI/main/"
+local repo = "https://raw.githubusercontent.com/Q2674791739/UI/main/Obsidian/"
 local Library = loadstring(game:HttpGet(repo .. "Library.lua"))()
 local ThemeManager = loadstring(game:HttpGet(repo .. "ThemeManager.lua"))()
 local SaveManager = loadstring(game:HttpGet(repo .. "SaveManager.lua"))()
